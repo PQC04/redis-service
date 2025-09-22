6d04d28627a23b3b00408a41dca42d7c2438b03a
PQC <PQC1105@163.com>
2025-09-22 14:07:24 +0800

    0922

---
+++ src/main/java/com/pqc/redisService/config/RedisAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.pqc.redisService.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.pqc.redisService")
+public class RedisAutoConfiguration {
+}
